1b38ae3f80bfbeb2788061ef44c2fbf530287ffe
Arvid98 <111972825+Arvid98@users.noreply.github.com>
2023-11-10 20:20:16 +0100

    Update PathNode.cs

--- GameJame2023Ht2d/Assets/Scripts/PathNode.cs
+++ GameJame2023Ht2d/Assets/Scripts/PathNode.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PathNode
 {
-    private Grid<PathNode> grid;
-    private int x;
-    private int y;
-    public PathNode(Grid<PathNode> grid, int x, int y)
-    {
-        this.grid = grid;
-        this.x = x;
-        this.y = y;
-    }
+    //private Grid<PathNode> grid;
+    //private int x;
+    //private int y;
+    //public PathNode(Grid<PathNode> grid, int x, int y)
+    //{
+    //    this.grid = grid;
+    //    this.x = x;
+    //    this.y = y;
+    //}
 }
 
