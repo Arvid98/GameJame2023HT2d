eb476dcc3b5baee46403730fe46c1f94503a62b3
kim-svedberg <110769011+kim-svedberg@users.noreply.github.com>
2023-11-10 21:26:06 +0100

    kuber
    Co-Authored-By: Martin Elovsson <112435323+Elovsson@users.noreply.github.com>

--- GameJame2023Ht2d/Assets/Scripts/FindTheName.cs
+++ GameJame2023Ht2d/Assets/Scripts/FindTheName.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class FindTheName : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject square;
+    Vector2 spawnPoint = new Vector2(-6.5f, 2.5f);
+
     void Start()
-    {
-        
+    {        
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+
+                SpawnObject(new Vector2(spawnPoint.x + i, spawnPoint.y - j));
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    void SpawnObject(Vector2 pos) 
+    { 
+        Instantiate(square, pos, Quaternion.identity);
     }
 }
