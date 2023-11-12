e0cc968205e773e5b8ce0e7c54157b67c44d6fb2
ViggeGee <victorgustafsson65@gmail.com>
2023-11-12 14:50:09 +0100

    yeehaw :8ball:

--- GameJame2023Ht2d/Assets/Scripts/Labyrint/PlayerVision.cs
+++ GameJame2023Ht2d/Assets/Scripts/Labyrint/PlayerVision.cs
@@ -10,8 +10,8 @@ public class PlayerVision : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Handles.color = new Color(50, 50,0, 0.3f);
-        Handles.DrawSolidDisc(transform.position, transform.up, fov);
+        //Handles.color = new Color(50, 50,0, 0.3f);
+        //Handles.DrawSolidDisc(transform.position, transform.up, fov);
     }
 
 
